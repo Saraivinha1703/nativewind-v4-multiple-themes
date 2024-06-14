d8ff9ebd48719c0cb1b315ae71d3fea43a6de076
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-06-14 21:00:53 +0100

    📦 NEW: Turning the App into a provider holder

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import "../global.css";
-import "tailwindcss/base";
-import "tailwindcss/components";
-import "tailwindcss/utilities";
 
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Theme } from "./components/themes/provider";
+import { Home } from "./screens/home";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-blue-500">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Theme>
+      <Home />
+    </Theme>
   );
 }
